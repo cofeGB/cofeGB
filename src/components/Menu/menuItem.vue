ce4d8fa1a3c5799a33b6120dcfdae19b325fc3fe
Pashkov Evgeniy <pashkoveval@gmail.com>
2021-12-04 20:51:34 +0300

    1

---
+++ src/components/Menu/menuItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="product-desc">
+    {{ item }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'menuItem',
+  props: ['item', 'img'],
+};
+</script>
---
+++ src/components/Menu/menuList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="cofMenu">
+    <p class="cofMenu-placehold" v-if="list.length === 0">Ничего не найдено</p>
+    <menu-item v-for="item of list" :item="item" :key="item.id_product" :img="imgUrl"></menu-item>
+  </div>
+</template>
+
+<script>
+import menuItem from './menuItem.vue';
+export default {
+  name: 'menuList',
+  props: ['list'],
+  components: {
+    menuItem,
+  },
+  data() {
+    return {
+      imgUrl: 'https://placehold.it/200x150',
+    };
+  },
+};
+</script>
