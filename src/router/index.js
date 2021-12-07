af7a6278b4fff2bdff39353dfc363581c24df6a9
Валерий <rasvv@mail.ru>
2021-12-07 21:57:24 +0300

    Добавил О нас и контакты

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue';
 import Delivery from '../views/Delivery.vue';
 import Menu from '../views/Menu.vue';
 import About from '../views/About.vue';
+import Contacts from '../views/Contacts.vue';
 import dish from '../components/dish.vue';
 
 Vue.use(VueRouter);
@@ -18,6 +19,11 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About,
+  },
+  {
+    path: '/contacts',
+    name: 'Contacts',
+    component: Contacts,
   },
   {
     path: '/delivery',
---
+++ src/views/Contacts.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container class="d-flex justify-center">
+    <v-card class="d-flex flex-column justify-space-between pa-2" width="960" height="auto">
+      <v-card-title class="d-flex justify-center header-text"> Кофейня "Бодрит"</v-card-title>
+
+      <div class="footer__contacts">
+        <div class="footer__contacts-left">
+          <div class="adress">г. Москва, Красная площадь, 1</div>
+          <div class="mail">
+            <a href="mailto:info@bodrit.ru">
+              <i class="far fa-envelope mar"></i>
+              info@bodrit.ru
+            </a>
+          </div>
+          <div class="phone1">
+            <a href="tel:+7-999-888-77-66">
+              <i class="fas fa-mobile-alt marbig"></i>+7-999-888-77-66
+            </a>
+          </div>
+        </div>
+
+        <div class="footer__map">
+          <iframe
+            class="footer__map-frame"
+            src="https://yandex.ru/map-widget/v1/-/CCUunWh~WA"
+            width="100%"
+            height="300"
+            frameborder="1"
+            allowfullscreen="true"
+          >
+          </iframe>
+        </div>
+      </div>
+
+      <v-card-text
+        class="d-flex justify-center text--primary align-center flex-grow-1 flex-shrink-0 text-h5 mx-auto"
+      >
+        Режим работы:
+      </v-card-text>
+      <div class="text-center">Ежедневно с 08-00 до 23-00</div>
+      <div class="text-center">Без выходных и праздников</div>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'About',
+};
+</script>
+
+<style lang="scss" scoped></style>
