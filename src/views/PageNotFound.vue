365a0c9fb6f093282dd3c6e5ab0d3febf9502b61
Aleksey Potapov <apotapovxx@mail.ru>
2021-12-08 22:32:19 +0300

    feat (app): add 404 page

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="text-center msg_text">
+      (404) We are sorry, but the page you requested was not found. Our specially trained team is
+      working on this problem.
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.msg_text {
+  color: #fff;
+}
+</style>
