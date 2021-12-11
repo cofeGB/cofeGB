4df285c236f3d7b78b6d3fd99d0f210db44152d6
Aleksey Potapov <apotapovxx@mail.ru>
2021-12-11 13:03:58 +0300

    chore (priv-order-desk): update orders mock data

--- src/mockdata/priv-order.js
+++ src/mockdata/priv-order.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * @typedef {Object} IDish
+ * @typedef {Object} Dish
  * @property {string} title
  * @property {string} id
  * @property {string} description
@@ -13,17 +13,75 @@
 
 /**
  * @typedef {Object} PrivOrder
- * @property {string} creationDateUTC - 2021-12-10T18:53:36Z
+ * @property {string} creationDateUTC - 'Sat, 11 Dec 2021 08:36:34 GMT'
  * @property {string} id
- * @property {IDish[]} dishList
+ * @property {Dish[]} dishList
  * @property {OrderStatus} status
  */
 
-function getCreationDate() {
-  const timeVariationMax = 
-  return Date.now();
+/**
+ * @returns {string}
+ */
+function getOrderCreationDate() {
+  const timeVariationMaxMs = 1000 * 60;
+  const randomDateMs = Date.now() + Math.round(timeVariationMaxMs * Math.random());
+  const randomDate = new Date(randomDateMs);
+  const randomDateUTCString = randomDate.toUTCString();
+  return randomDateUTCString;
 }
 
+/** @type {string[]} */
+const usedOrderIds = [];
+
+/** @type {string[]} */
+const usedDishIds = [];
+
+/**
+ * @param {string[]} usedIds - array to store all ids and prevent id duplicates
+ * @return {string} next id
+ */
+function getUniqueId(usedIds) {
+  /** @type {string} */
+  const nextId = usedIds.length > 0 ? `${+usedIds[usedIds.length - 1].id + 1}` : '1';
+  usedIds.push(nextId);
+  return nextId;
+}
+
+/**
+ * @return {Dish} random dish object
+ */
+function getRandomDish() {
+  /** @type {string} */
+  const id = `D${getUniqueId(usedDishIds)}`;
+
+  /** @type {Dish} */
+  const dish = {
+    title: `Dish ${id} title`,
+    id: `D${getUniqueId(usedDishIds)}`,
+    description: `Dish ${id} description atem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet`,
+  };
+  return dish;
+}
+
+/**
+ * @returns {PrivOrder}
+ */
 export function getRandomPrivOrder() {
-  return {};
+  /** @type {number} */
+  const dishCount = 1 + Math.round(4 * Math.random());
+
+  /** @type {Dish[]} */
+  const dishList = [];
+  for (let i = 0; i < dishCount; i++) {
+    dishList.push(getRandomDish());
+  }
+
+  /** @type {PrivOrder} */
+  const order = {
+    creationDateUTC: getOrderCreationDate(),
+    id: `Z${getUniqueId(usedOrderIds)}`,
+    dishList,
+    status: 'pending',
+  };
+  return order;
 }
