15e8764e930344b59f9f2b1e0b09cc3f95de99c3
rasvv <rasvv@mail.ru>
2021-12-14 15:48:10 +0300

    Перед доработкой About

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,6 +10,7 @@
           </div>
           <div class="text-center text-h3 ma-7">Наша команда</div>
           <v-carousel
+            :cycle="cycled"
             show-arrows-on-hover
             hide-delimiters
             class="d-flex justify-center"
@@ -43,25 +44,24 @@
                 <ShowReviews
                   v-if="showReviewsVisible"
                   @changeShowReviewsVisible="changeShowReviewsVisible"
-                  :e_id="item.id"
+                  :employeeReviews="employeeReviews"
                 />
-                <v-btn
-                  depressed
-                  color="primary"
-                  v-bind="attrs"
-                  v-on="on"
-                  @click="employee_id = item.id"
-                >
-                  Оставить отзыв
-                </v-btn>
-                <v-btn
-                  v-if="!showReviewsVisible"
-                  @click="changeShowReviewsVisible"
-                  depressed
-                  color="primary"
-                >
-                  Посмотреть отзывы
-                </v-btn>
+                <v-card-actions>
+                  <v-btn
+                    color="primary"
+                    v-bind="attrs"
+                    v-on="on"
+                    @click="
+                      employee_id = item.id;
+                      cycled = false;
+                    "
+                  >
+                    Оставить отзыв
+                  </v-btn>
+                  <v-btn @click="changeShowReviewsVisible(item.id)" color="primary">
+                    Посмотреть отзывы
+                  </v-btn>
+                </v-card-actions>
               </v-card>
             </v-carousel-item>
           </v-carousel>
@@ -71,9 +71,14 @@
       <v-card class="d-flex flex-column pa-10">
         <v-card-actions d-flex justify="end" class="pa-0">
           <v-spacer></v-spacer>
-          <v-btn icon @click="dialog = false">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }" position-x="5" position-y="5">
+              <v-btn icon @click="dialog = false" v-bind="attrs" v-on="on">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+            </template>
+            <span>Закрыть форму</span>
+          </v-tooltip>
         </v-card-actions>
         <h3>Поставьте оценку</h3>
         <v-text-field v-model="name" label="Введите свое имя"></v-text-field>
@@ -89,6 +94,39 @@
         <div black--text>Ваша оценка ({{ rating }})</div>
         <v-textarea solo v-model="description" name="input-7-4" label="Введите отзыв"></v-textarea>
         <v-btn depressed color="primary" @click="addReview">Оставить отзыв</v-btn>
+        <v-overlay :absolute="absolute" :value="overlay" @click="overlay = false">
+          Не все поля заполнены
+        </v-overlay>
+      </v-card>
+      <v-card class="d-flex flex-column pa-10">
+        <v-card-actions d-flex justify="end" class="pa-0">
+          <v-spacer></v-spacer>
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }" position-x="5" position-y="5">
+              <v-btn icon @click="dialog = false" v-bind="attrs" v-on="on">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+            </template>
+            <span>Закрыть форму</span>
+          </v-tooltip>
+        </v-card-actions>
+        <h3>Поставьте оценку</h3>
+        <v-text-field v-model="name" label="Введите свое имя"></v-text-field>
+        <h5>Выберите необходимое количество звезд</h5>
+        <v-rating
+          v-model="rating"
+          background-color="green lighten-3"
+          color="green"
+          empty-icon="$ratingFull"
+          half-increments
+          hover
+        ></v-rating>
+        <div black--text>Ваша оценка ({{ rating }})</div>
+        <v-textarea solo v-model="description" name="input-7-4" label="Введите отзыв"></v-textarea>
+        <v-btn depressed color="primary" @click="addReview">Оставить отзыв</v-btn>
+        <v-overlay :absolute="absolute" :value="overlay" @click="overlay = false">
+          Не все поля заполнены
+        </v-overlay>
       </v-card>
     </v-dialog>
   </v-row>
@@ -104,6 +142,8 @@ export default {
   data() {
     return {
       globalName: store.state.globalConst.brandName,
+      reviews: store.state.reviewsList,
+      employeeReviews: [],
       items: store.state.employee,
       showReviewsVisible: false,
       dialog: false,
@@ -112,6 +152,9 @@ export default {
       rating: 5,
       date: new Date().toLocaleDateString(),
       description: '',
+      absolute: true,
+      overlay: false,
+      cycled: true,
     };
   },
   components: {
@@ -119,8 +162,13 @@ export default {
   },
   methods: {
     ...mapActions(['ADD_REVIEW']),
-    changeShowReviewsVisible() {
+    changeShowReviewsVisible(id) {
+      console.log(this.reviews);
+      console.log(id);
+      this.employeeReviews = this.reviews.filter(emp => emp.employee_id === id);
+      console.log(this.employeeReviews);
       this.showReviewsVisible = !this.showReviewsVisible;
+      this.cycled = false;
     },
     addReview() {
       let { employee_id, name, rating, date, description } = this;
@@ -130,7 +178,7 @@ export default {
         this.dialog = false;
         this.addReviewsVisible = !this.addReviewsVisible;
       } else {
-        alert('Заполнены не все поля');
+        this.overlay = true;
       }
     },
   },
--- src/views/ShowReviews.vue
+++ src/views/ShowReviews.vue
@@ -1,26 +1,35 @@
 <template>
   <v-container class="d-flex flex-column">
-    <h3>Поставьте оценку</h3>
-    <v-text-field v-model="name" label="Введите свое имя"></v-text-field>
-    <h5>Выберите необходимое количество звезд</h5>
-    <v-rating
-      v-model="rating"
-      background-color="green lighten-3"
-      color="green"
-      empty-icon="$ratingFull"
-      half-increments
-      hover
-    ></v-rating>
-    <div black--text>Ваша оценка ({{ rating }})</div>
-    <v-textarea solo v-model="description" name="input-7-4" label="Введите отзыв"></v-textarea>
-    <v-btn depressed color="primary" @click="changeReviewsVisible">Оставить отзыв</v-btn>
+    <h3>Работник</h3>
+    <v-carousel
+      cycle
+      show-arrows-on-hover
+      hide-delimiters
+      class="d-flex justify-center"
+      height="auto"
+    >
+      <v-carousel-item v-for="(item, i) in employeeReviews" :key="i" height="auto">
+        <v-card class="d-flex flex-column justify-center align-center pa-3" width="500">
+          <v-rating
+            v-model="item.rating"
+            background-color="green lighten-3"
+            color="green"
+            empty-icon="$ratingFull"
+            half-increments
+          ></v-rating>
+          <div black--text>Ваша оценка ({{ item.rating }})</div>
+          <div black--text>{{ item.description }}</div>
+        </v-card>
+      </v-carousel-item>
+    </v-carousel>
   </v-container>
 </template>
+
 <script>
 import { mapActions } from 'vuex';
 export default {
-  name: 'reviews',
-  props: ['e_id'],
+  name: 'showreviews',
+  props: ['employeeReviews'],
   data() {
     return {
       employee_id: 0,
@@ -31,7 +40,7 @@ export default {
     };
   },
   methods: {
-    ...mapActions(['ADD_REVIEW']),
+    ...mapActions(['GET_EMPLOYEE_REVIEWS_LIST']),
     changeReviewsVisible() {
       this.employee_id = this.e_id;
       let { employee_id, name, rating, date, description } = this;
@@ -40,6 +49,9 @@ export default {
       this.$emit('changeReviewsVisible');
     },
   },
+  // created: {
+  //   reviews = employeeReviews
+  // }
 };
 </script>
 <style lang="scss" scoped></style>
