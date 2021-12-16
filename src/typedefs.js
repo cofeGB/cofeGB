776c440d8bb92208d2d79472850bcd5cbe9e66b5
Aleksey Potapov <alex1364@mail.ru>
2021-12-16 16:24:09 +0300

    chore (types): add itemOrder property into NavMenuItem

--- src/typedefs.js
+++ src/typedefs.js
@@ -111,6 +111,7 @@
  * @typedef {Object} NavMenuItem - элемент навигационного меню приложения
  * @property {string} title - название пункта навигационного меню, как его видит пользователь
  * @property {string} path - полный путь роутера '/menu/sandwiches'|'/menu/salads'...
+ * @property {number} itemOrder - порядковый номер данного элемента в меню, целые числа 0,1,2... 
  */
 
 /**
