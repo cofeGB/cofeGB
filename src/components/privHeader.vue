f00613504dd9ce7025784462beddb2d19d507172
Pashkov Evgeniy <pashkoveval@gmail.com>
2021-12-19 17:39:20 +0300

    f

---
+++ src/components/privHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header">
+    <v-btn color="gray" class="ma-2" dark>Категории</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PrivatHeader',
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  background: rgba(48, 24, 13, 0.5);
+  width: 100%;
+}
+</style>
