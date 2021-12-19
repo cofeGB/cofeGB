a18e8c3e32a5a3bcf77e4cca09e920678aec65b5
Pashkov Evgeniy <pashkoveval@gmail.com>
2021-12-19 16:27:21 +0300

    f

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -37,7 +37,6 @@ export default {
     ...mapActions(['GET_MENU', 'GET_CATEGORIES']),
   },
   created() {
-    // this.GET_CATEGORIES()
     this.GET_MENU(this.$route.params.category);
   },
   mounted() {
