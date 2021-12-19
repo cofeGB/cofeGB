91cb5a306fd67b7afff26c891b0af0eecccb90d8
Pashkov Evgeniy <pashkoveval@gmail.com>
2021-12-19 17:24:16 +0300

    basket болванка

--- src/components/CofBasket/CofBasket.vue
+++ src/components/CofBasket/CofBasket.vue
@@ -1,16 +1,20 @@
 <template>
   <Modal :activator="basket" @close="close">
     <template #content>
-      <v-flex class="classes">
-        <v-toolbar color="primary">
-          <v-spacer></v-spacer>
-          <v-toolbar-title class="mr-3">Корзина</v-toolbar-title>
-          <v-btn icon @click="close">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-        </v-toolbar>
-      </v-flex>
-      <div class="body">ffffffffff</div>
+      <div class="classes">
+        <v-flex>
+          <v-toolbar color="primary">
+            <v-spacer></v-spacer>
+            <v-toolbar-title class="mr-3">Корзина</v-toolbar-title>
+            <v-btn icon @click="close">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-toolbar>
+        </v-flex>
+        <v-row>
+          <div class="body">ffffffffff</div>
+        </v-row>
+      </div>
     </template>
   </Modal>
 </template>
@@ -34,5 +38,6 @@ export default {
 .classes {
   background: rgba(0, 0, 0, 0.9) !important;
   height: 100vh !important;
+  color: #fff !important;
 }
 </style>
