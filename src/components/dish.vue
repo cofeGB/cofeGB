8c1928544c3314b3731435198255694e4fd83bd0
Pashkov Evgeniy <pashkoveval@gmail.com>
2021-12-19 16:30:34 +0300

    f

--- src/components/dish.vue
+++ src/components/dish.vue
@@ -59,9 +59,12 @@
           <h4>Описание</h4>
           <p>{{ menuItem.description }}</p>
         </div>
-        <div class="dish__main_description">
-          <h4>Описание:</h4>
-          <span>{{ menuItem.description }}</span>
+        <div class="dish__main_cpfc">
+          <div v-for="(i, index) of menuItem.calories" :key="index">
+            <span>{{ i.procents }}</span>
+            <br />
+            <span>{{ i.title }}</span>
+          </div>
         </div>
       </div>
       <div v-else>
