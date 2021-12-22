d1c595e69c6f9ed8ca10d3c5887b57b97ba1a3ad
Pashkov Evgeniy <pashkoveval@gmail.com>
2021-12-22 21:46:55 +0300

    Добавлена карта  в корзину

--- src/components/CofBasket/CofBasketPastOrdersList.vue
+++ src/components/CofBasket/CofBasketPastOrdersList.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-list class="list" dark>
-    <v-list-item-group class="list-order">
-      <v-list-item class="list-item my-2 py-1">
-        <span> История заказов доступна после авторизации. </span>
-        <span> Перейти к авторизации? </span>
-      </v-list-item>
-    </v-list-item-group>
-  </v-list>
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15968.205551418063!2d30.387016544514474!3d59.981483825584206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x469633c0a2ad668d%3A0x7e0f8c09698ee03e!2z0JrRg9GI0LXQu9C10LLRgdC60LDRjyDQtNC-0YAuLCAzINC60L7RgNC_0YPRgSAzLCDQodCw0L3QutGCLdCf0LXRgtC10YDQsdGD0YDQsywgMTk1MjIw!5e0!3m2!1sru!2sru!4v1640198380164!5m2!1sru!2sru"
+    width="600"
+    height="450"
+    style="border: 0"
+    allowfullscreen=""
+    loading="lazy"
+  ></iframe>
 </template>
 
 <script>
