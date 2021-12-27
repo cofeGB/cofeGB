69ddc94f665cd8ea9e64782160c9de07b77b9f8d
Aleksey Potapov <alex1364@mail.ru>
2021-12-27 16:32:21 +0300

    feat (server-deploy): setup server deploy to heroku

--- server/server.js
+++ server/server.js
@@ -106,7 +106,7 @@ app.get('/api/loyalty', (req, res) => {
   });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening ${port} port`);
 });
